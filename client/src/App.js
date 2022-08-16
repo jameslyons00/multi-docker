59fc95db9bde9b3ec5e23dc52d13aad994d2bf15
zlyojam <james.lyons.ext@ericsson.com>
2022-08-16 10:16:47 +0100

    Changed greeting message in app js
    Added AWS keys to Travis
    
    Changed docker-compose yml

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Multi-Docker App
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
